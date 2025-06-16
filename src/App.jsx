e0f1ef65383a7810103cdc63d7204482c17d5349
GurhanAksoy <aksoy75@gmail.com>
2025-06-17 00:36:58 +0300

    JSX hatası düzeltildi, test yapısı yeniden yüklendi

--- src/App.jsx
+++ src/App.jsx
@@ -26,4 +26,8 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/whitepaper" elemen
+        <Route path="/whitepaper" element={<Whitepaper />} />
+      </Routes>
+    </Router>
+  );
+}
