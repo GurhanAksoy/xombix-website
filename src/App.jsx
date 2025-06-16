050383a081a9b6419e309054c8bd67ef9ce0a967
GurhanAksoy <aksoy75@gmail.com>
2025-06-17 02:10:18 +0300

    Presale sayfası eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Whitepaper from "./components/Whitepaper";
-import Navbar from "./components/Navbar"; // ← Yeni eklenen üst menü
+import Navbar from "./components/Navbar";
+import Presale from "./pages/Presale";
 
 export default function App() {
   return (
     <Router>
-      <Navbar /> {/* Bu menü her sayfada üstte kalır */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/whitepaper" element={<Whitepaper />} />
+        <Route path="/presale" element={<Presale />} />
       </Routes>
     </Router>
   );
