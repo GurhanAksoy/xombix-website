c8b155d2098f40c00804847d3a18789038cb7e7a
GurhanAksoy <aksoy75@gmail.com>
2025-06-17 00:22:18 +0300

    geçici dosya ismi düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Whitepaper from "./components/WhitepaperTEMP";
+import Whitepaper from "./components/Whitepaper";
 
 export default function App() {
   return (
