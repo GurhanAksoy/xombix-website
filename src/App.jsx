679f1f9073d769acf427be7548b95b9b95b44e2e
GurhanAksoy <aksoy75@gmail.com>
2025-06-18 00:40:11 +0300

    app ve index arkaplan düzenlemesi

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,6 @@ export default function App() {
       {/* 🎵 XombiX Anthem - sayfa değişiminden etkilenmez */}
       <audio ref={audioRef} src="/xombix.mp3" preload="auto" />
 
-      {/* 🌌 Yıldızlı Arka Plan */}
-      <div className="stars"></div>
-
       {/* 🔗 Navbar ve Sayfalar */}
       <Navbar />
       <Routes>
