8f0521f56709a5cac5334acf4f10782cef9b0eee
GurhanAksoy <aksoy75@gmail.com>
2025-07-03 00:55:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,19 @@
-import React, { useRef, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Whitepaper from "./Whitepaper";
+import XombixAnthem from "./XombixAnthem";
 
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Whitepaper from "./components/Whitepaper";
-import Presale from "./pages/Presale";
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
-export default function App() {
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    const playMarsh = () => {
-      if (audioRef.current) {
-        audioRef.current.volume = 1;
-        audioRef.current.play().catch((err) => {
-          console.error("Marş başlatılamadı:", err);
-        });
-        window.removeEventListener("click", playMarsh);
-      }
-    };
-
-    window.addEventListener("click", playMarsh);
-
-    return () => {
-      window.removeEventListener("click", playMarsh);
-    };
-  }, []);
-
+function App() {
   return (
-    <Router>
-      {/* Scroll to Top on Route Change */}
-      <ScrollToTop />
-
-      {/* 🎵 XombiX Anthem - sayfa değişiminden etkilenmez */}
-      <audio ref={audioRef} src="/xombix.mp3" preload="none" aria-hidden="true" / style={{ display: "none" }}>
-
-      {/*  Arka plan yıldız efekti */}
-      <div className="stars"></div>
-
-      {/* 🔗 Navbar ve Sayfalar */}
+    <div>
+      <audio src="/xombix.mp3" autoPlay loop muted style={{ display: "none" }}></audio>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/whitepaper" element={<Whitepaper />} />
-        <Route path="/presale" element={<Presale />} />
-      </Routes>
-    </Router>
+      <Home />
+      <Whitepaper />
+      <XombixAnthem />
+    </div>
   );
 }
+
+export default App;
