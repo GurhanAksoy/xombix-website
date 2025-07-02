8c7968c1b6e85f25918259a1040abdcc81cce523
GurhanAksoy <aksoy75@gmail.com>
2025-07-03 00:32:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
-import React, { useRef, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Whitepaper from "./components/Whitepaper";
-import Presale from "./pages/Presale";
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
-export default function App() {
-  
-  useEffect(() => {
-    const playMarsh = () => {
-      if (audioRef.current) {
-        audioRef.current.volume = 1;
-        audioRef.current.play().catch((err) => {
-          console.error("Marş başlatılamadı:", err);
-        });
-        window.removeEventListener("click", playMarsh);
-      }
-    };
-
-    window.addEventListener("click", playMarsh);
-
-    return () => {
-      window.removeEventListener("click", playMarsh);
-    };
-  }, []);
-
+function App() {
   return (
-    <Router>
-      {/* Sayfa değişiminde yukarı kaydır */}
-      <ScrollToTop />
-
-      {/* 🎵 XombiX Anthem - performans için preload kaldırıldı */}
-      <audio
-        ref={audioRef}
-        src="/xombix.mp3"
-        preload="none"
-        style={{ display: "none" }}
-        aria-label="XombiX Anthem"
-      />
-
-      {/* Arka plan yıldız efekti */}
-      <div className="stars" role="presentation" />
-
-      {/* Navigasyon ve Sayfa Yönlendirme */}
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/whitepaper" element={<Whitepaper />} />
-        <Route path="/presale" element={<Presale />} />
-      </Routes>
-    </Router>
+      <Home />
+    </>
   );
 }
+
+export default App;
